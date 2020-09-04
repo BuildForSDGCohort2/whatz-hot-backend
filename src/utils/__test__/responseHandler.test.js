f9f41cd9c9157646edb326adf5255628ed04e983
Dave <callremz@gmail.com>
2020-09-04 08:07:16 +0100

    chore: add default response handler and tests

---
+++ src/utils/__test__/responseHandler.test.js
@@ -0,0 +1,17 @@
+import responseHandler from "../responseHandler";
+import express from "express";
+import request from "supertest";
+
+const app = express();
+app.get("/", (req, res) => {
+  return responseHandler(res, 200, ["some data"], "Some optional message");
+});
+
+describe("Unit test responseHandler", () => {
+  it("should return formatted response", async () => {
+    const response = await request(app).get("/");
+    expect(response.status).toBe(200);
+    expect(response.body.data.length).toBe(1);
+    expect(response.body.message).toBe("Some optional message");
+  });
+});
